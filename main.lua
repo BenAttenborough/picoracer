41379d90533c74dcf29eca4a86a39faf35d587e2
Ben Attenborough <battenborough@gmail.com>
2022-06-03 15:27:51 +0100

    Tidy up curve colouring algorthym

--- main.lua
+++ main.lua
@@ -12,9 +12,35 @@ function _init()
     minXCurve = 0
     curbWidth = 4
     logfile = "log.txt"
+    count = 1
+    slowCount = 1
+    movementCount = 1
+    Dlog("Initialise log", true)
 end
 
 function _update()
+    input()
+    count += 1
+    count = count % 10
+    if count % 5 == 0 then
+        slowCount += 1
+        slowCount = slowCount % 5
+    end
+    movementCount = movementCount % 5
+    
+end
+
+function _draw()
+    cls(3)
+    drawLeftRoadEdge()
+    drawCentralLine()
+    drawRightRoadEdge()
+    drawSky()
+    -- print(slowCount,5,5,7)
+    print(movementCount,5,5,7)
+end
+
+function input()
     if (btn(0)) then
         leftBezierAnchorX -= 1
         rightBezierAnchorX -= 1
@@ -31,23 +57,16 @@ function _update()
         rightBezierTopX -= horzMovementInterval
         centralBezierTopX -= horzMovementInterval
     end
-    if (btn(2)) then
-        bezierAnchorY -= 1
-        bezierAnchorY -= 1
+    if (btnp(2)) then
+        movementCount += 1
+        -- bezierAnchorY -= 1
+        -- bezierAnchorY -= 1
     end
-    if (btn(3)) then
-        bezierAnchorY += 1
-        bezierAnchorY += 1
+    if (btnp(3)) then
+        movementCount -= 1
+        -- bezierAnchorY += 1
+        -- bezierAnchorY += 1
     end
-end
-
-function _draw()
-    cls(3)
-    drawLeftRoadEdge()
-    drawCentralLine()
-    drawRightRoadEdge()
-    drawSky()
-    
 end
 
 function drawSky()
@@ -97,21 +116,47 @@ function drawqbc(x1,y1,x2,y2,x3,y3,n,w,c)
     end
 end
 
-function toggleColour(originalCol,c1,c2)
-    if originalCol == c1 then return c2 else return c1
-    end
+-- function toggleColour(originalCol,c1,c2)
+--     if originalCol == c1 then return c2 else return c1
+--     end
+-- end
+
+function toggleCurbColours()
+    local temp = c1
+    c1 = c2
+    c2 = temp
 end
 
 function drawqbcAlternating(x1,y1,x2,y2,x3,y3,n,w,c1,c2)
+    -- if movementCount == 0 then 
+    --     local temp = c1
+    --     c1 = c2
+    --     c2 = temp
+    -- end
+    local initY = qbcvector(y1,y2,y3,1/n)
+    -- print("initY",5,10,7)
+    -- Dlog(initY)
     local colour = c1
+    -- if slowCount == 0 then
+    --     colour = c1
+    -- else
+    --     colour = c2
+    -- end
+    -- if slowCount == 4 then toggleColour(colour,c1,c2) end
     for i = 1,n do 
         local t = i/n
         local x = qbcvector(x1,x2,x3,t)
         local y = qbcvector(y1,y2,y3,t)
-        if flr(y) % 5 == 1 then colour = toggleColour(colour,c1,c2) end
+        -- Dlog("Y: " .. y .. ", flr(y): " .. flr(y) .. "Toogle?: " .. (flr(y) % 5 == 1 and "True" or "false"))
+        -- Dlog("Y: " .. y .. ", flr(y): " .. flr(y) .. " Last digit: " .. flr(flr(y)%10))
+        -- if flr(y) % 5 == 1 then colour = toggleColour(colour,c1,c2) end
+        
+        if flr(flr(y)%10) >= movementCount and flr(flr(y)%10) < movementCount + 5 then colour = c1 else colour = c2 end
         for j = 0,(w-1) do
             pset(x+j,y,colour)
         end
+        
+
     end
 end
 
